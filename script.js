73da4f8ac5a14e8c79e7b00e15fdcea862b45f3a
Sichkar Yurii <si4kar2017@gmail.com>
2022-06-11 14:38:56 +0300

    Lesson_22

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 calculator = require('./calculator');
 
 calculator.set(10);
-console.log(calculator.add(2)) // 12
-console.log(calculator.sub(5)) // 7
-console.log(calculator.mult(4)) // 28
-console.log(calculator.div(2)) // 14
+console.log(calculator.add(2));
+console.log(calculator.sub(5));
+console.log(calculator.mult(4));
+console.log(calculator.div(2));
