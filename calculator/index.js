4a67a8f3d9e3689bb6a4789427344e266e0f82e4
Sichkar Yurii <si4kar2017@gmail.com>
2022-06-11 14:29:29 +0300

    Lesson_22

---
+++ calculator/index.js
@@ -0,0 +1,14 @@
+const set = require('./set');
+const add = require('./add');
+const div = require('./divide');
+const mult = require('./mult');
+const sub = require('./sub');
+
+
+module.exports = {
+    set: set,
+    add: add,
+    div: div,
+    mult: mult,
+    sub: sub,    
+}
---
+++ calculator/sub.js
@@ -0,0 +1,1 @@
+module.exports = (b) => result -= b;
